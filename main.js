5d3f927a3a6cd37a599054d01d246d7e4c6567f2
internetisgone <schumann777@proton.me>
2023-03-19 16:27:20 +0800

    add discord bot under projects

--- main.js
+++ main.js
@@ -38,26 +38,3 @@ function scrollToTop()
 {
     window.scrollTo({top: 0, behavior: 'smooth'});
 }
-
-let imgIndex = 0
-function changeImage(el, delta)
-{
-    let carousel = el.parentElement
-    let imgElements = carousel.getElementsByTagName("img")
-
-    if (delta > 0) //index++
-    {
-        imgIndex = (imgIndex + 1 > imgElements.length - 1)? 0 : (imgIndex + 1);
-        // imgElements.item(imgIndex).style.transform = "translateX(100%)"
-    }
-    else //index--
-    {
-        imgIndex = (imgIndex - 1 < 0)? (imgElements.length - 1) : (imgIndex - 1);
-        // imgElements.item(imgIndex).style.transform = "translateX(-100%)"
-    }
-    
-    console.log("delta = " + delta + ", img index = " + imgIndex + " out of " + imgElements.length)
-    //get cur index (check style.display)?
-    //change index n display
-    
-}
