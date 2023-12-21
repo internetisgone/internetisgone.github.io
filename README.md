6b0a6fb5c385657994b11a5973fd1e347a6c35cb
internetisgone <schumann777@proton.me>
2023-12-21 15:02:48 +0800

    cleanup

--- README.md
+++ README.md
@@ -3,8 +3,9 @@
 static site generated with [staticjinja](https://github.com/staticjinja/staticjinja)<br>
 
 ```
+cd src
 python3 -m venv .venv
 . .venv/bin/activate
 pip install -r requirements.txt
 ```
-run `generate.py` to generate pages in `/templates` to root dir
+run `generate.py` to generate pages in `src/templates` to root dir
--- src/generate.py
+++ src/generate.py
@@ -1,7 +1,7 @@
 from staticjinja import Site
 import os
 
-PHOTOS_PATH = "photos"
+PHOTOS_PATH = "../photos"
 
 def get_photos(photos_path):
     photonames = os.listdir(photos_path)
@@ -9,12 +9,15 @@ def get_photos(photos_path):
         if photoname.startswith("."):
             photonames.remove(photoname)
     # append path 
-    photonames = [photos_path + "/" + photoname for photoname in photonames]
+    photonames = [photos_path[3:] + "/" + photoname for photoname in photonames]
     print(photonames)
     # sort by name
     return sorted(photonames) 
 
 if __name__ == "__main__":
     photos = get_photos(PHOTOS_PATH)
-    site = Site.make_site( contexts = [ ("photos.html", {"photos": photos }) ] )
+    site = Site.make_site( 
+            contexts = [ ("photos.html", {"photos": photos }) ],
+            outpath = "../"
+        )
     site.render(use_reloader=True)
