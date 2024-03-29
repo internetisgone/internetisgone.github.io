fbf8b627a76ddeea614cffeb03b003480d90a835
internetisgone <108741687+internetisgone@users.noreply.github.com>
2024-03-29 19:02:02 +0800

    add art pic links

--- src/generate.py
+++ src/generate.py
@@ -2,8 +2,32 @@ from staticjinja import Site
 import os
 
 PHOTOS_PATH = "../photos"
-ART_PICS = ["img/limbb.png", "img/bttp.png", "img/gg.png", "img/suspensionn.png", "img/knivess.png", "img/footwork_hobbes_logo.png" ]
-
+ART_PICS = [
+    {
+        "path": "img/limbb.png",
+        "link": "img/limbb.png"
+    },
+    {
+        "path": "img/bttp.png",
+        "link": "https://eternal100000000.bandcamp.com/album/thegn-silver-blade-shattered-mirror-deceiver"
+    },
+    {
+        "path": "img/gg.png",
+        "link": "img/gg.png"
+    },
+    {
+        "path": "img/suspensionn.png",
+        "link": "img/suspensionn.png"
+    },
+    {
+        "path": "img/knivess.png",
+        "link": "img/knivess.png"
+    },
+    {
+        "path": "img/footwork_hobbes_logo.png",
+        "link": "https://www.hkcr.live/residents/the-footwork-program"
+    } 
+]
 def get_photos(photos_path):
     photonames = os.listdir(photos_path)
     for photoname in photonames:
